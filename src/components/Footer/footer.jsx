cea7a4352b061a4ca3ef50b4563d0c1f4cdea9ef
ashish-gopalika <meashishgopalika@gmail.com>
2022-08-12 17:24:36 +0530

    added github repo link

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -35,7 +35,8 @@ export default function Footer() {
               <a target="_blank" href={SOCIALS.instagram}><i class="iconlink fab fa-instagram"></i></a>
               <a target="_blank" href={SOCIALS.linkedin}><i class="iconlink fab fa-linkedin-in"></i></a>
               <a target="_blank" href={SOCIALS.email}><i class="iconlink fa-solid fa-envelope"></i></a>
-              <a target="_blank" href={SOCIALS.github}><i class="iconlink fab fa-solid fa-github"></i></a>
+              <a target="_blank" href={SOCIALS.github}><i class="fab git fa-github"></i></a>
+
             </div>
           </div>
 
